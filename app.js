0f3d03a7c98fad2af36424f6f6d2a70684ad59fe
Emmy <cyubahiroemmy12@gmail.com>
2026-01-14 09:38:59 +0200

    Working on my first Nodejs files

---
+++ app.js
@@ -0,0 +1,6 @@
+const add = require('./math');
+
+console.log(add(5,10));
+
+const log = require('./logger');
+log('Hello Web3');
---
+++ logger.js
@@ -0,0 +1,6 @@
+const url = 'https://www.google.com';
+function log(message){
+    console.log(message);
+}
+
+module.exports = log;
