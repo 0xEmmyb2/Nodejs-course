ab76b544a592b72fb0d6fbd54ed469464cc8dbdd
Emmy <cyubahiroemmy12@gmail.com>
2026-01-31 06:49:44 +0200

    Updated

--- API/app.js
+++ API/app.js
@@ -2,6 +2,26 @@ const EventEmmitter = require('events');
 
 const eventEmitter = new EventEmmitter();
 
-eventEmitter.on('tutorial', () => {
-    console.log("Tutorial event has occured.")
+eventEmitter.on('tutorial', (num1, num2) => {
+    console.log(num1 + num2);
 });
+
+eventEmitter.emit('tutorial', 1, 2);
+
+class Person extends EventEmmitter{
+    constructor(name){
+        super();
+        this._name = name;
+    }
+
+    get name(){
+        return this._name;
+    }
+}
+
+let pedro = new Person('Pedro');
+pedro.on('name', ()=>{
+    console.log(`My name is ${pedro.name}`);
+});
+
+pedro.emit('name');
