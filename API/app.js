7c7b34553474a078ffad012e00e05e3ad9f13361
Emmy <cyubahiroemmy12@gmail.com>
2026-01-31 07:15:43 +0200

    Updated

--- API/app.js
+++ API/app.js
@@ -1,5 +1,12 @@
 const EventEmmitter = require('events');
 const readline = require('readline');
+const r1 = readline.createInterface({input : process.stdin,
+    output: process.stdout
+});
+
+let num1 = Math.floor((Math.random() * 10) + 1);
+let num2 = Math.floor((Math.random() * 10) + 1);
+
 
 const eventEmitter = new EventEmmitter();
 
