73b9eb17d84054e1921eed9d4f9b1000df932359
Emmy <cyubahiroemmy12@gmail.com>
2026-02-09 14:10:36 +0200

    Updated

--- app.js
+++
@@ -1,7 +0,0 @@
-import { add } from './math.js';
-import { log } from './logger.js';
-
-console.log(add(5,10));
-
-
-log('Hello Web3');
--- logger.js
+++
@@ -1,5 +0,0 @@
-const url = 'https://www.google.com';
-export const log = (message) => {
-    console.log(message);
-}
-
