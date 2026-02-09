dd984a085600a47a08c576c4cfb66a24a42ab277
Emmy <cyubahiroemmy12@gmail.com>
2026-02-09 19:55:15 +0200

    Updated

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require ('express');
 const app = express()
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+    console.log('Server is running on port 3000');
 });
 
 app.get('/', (req, res) => {
-    res.send("Hello from the Node API Server Updated")
+    res.send("Hello from the Node API Server Updated");
 })
