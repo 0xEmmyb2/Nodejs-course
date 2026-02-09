5d9b63ae653a57082cd25a111b6dfe4fedfba961
Emmy <cyubahiroemmy12@gmail.com>
2026-02-09 20:22:10 +0200

    Updated

--- index.js
+++ index.js
@@ -11,3 +11,6 @@ app.get('/', (req, res) => {
 });
 
 mongoose.connect("mongodb+srv://cyubahiroemmy12_db_user:cUgTzCHEdTeKu757@backenddb.ugxei1n.mongodb.net/Node-API?appName=BackendDB")
+.then(() => {
+    console.log("Connected to database!");
+})
