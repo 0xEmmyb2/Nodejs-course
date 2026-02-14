bfefc09ad800cbe88f911f6f8d9d4b9cd1b91f37
Emmy <cyubahiroemmy12@gmail.com>
2026-02-14 10:45:10 +0200

    Updated

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require ('express');
 const mongoose = require('mongoose');
 const app = express();
 
+const dbUser = 'cyubahiroemmy12_db_user';
+const dbPass = 'A%40barca100%25';
+const cluster = 'BackendDB';
+
 app.get('/', (req, res) => {
     res.send("Hello from the Node API Server Updated");
 });
 
-mongoose.connect("mongodb+srv://cyubahiroemmy12_db_user:A@barca100%@backenddb.ugxei1n.mongodb.net/?appName=BackendDB")
+mongoose.connect("mongodb+srv://cyubahiroemmy12_db_user:A%40barca100%25@backenddb.ugxei1n.mongodb.net/?appName=BackendDB")
 .then(() => {
     console.log("Connected to database!");
     app.listen(3000, () => {
