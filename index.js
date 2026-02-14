cd7685917b379c4787533d8508a2b1db2987c51b
Emmy <cyubahiroemmy12@gmail.com>
2026-02-14 15:26:52 +0200

    Reading api all

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const Product = require('./models/product.model')
+const {Product} = require('./models/product.model')
 
 const dbUser = "cyubahiroemmy12_db_user";
 const dbPass = "cBCTbXzeXmMtkJUs";
--- models/product.model.js
+++ models/product.model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const ProductSchema = mongoose.Schema(
+const productSchema = mongoose.Schema(
     {
         name : {
             type: String,
@@ -27,6 +27,6 @@ const ProductSchema = mongoose.Schema(
     }
 );
 
-const Product = mongoose.model("Product", ProductSchema);
+const Product = mongoose.model("Product", productSchema);
 
 module.export = Product;
