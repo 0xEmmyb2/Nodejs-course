dbd70ff63ba3dd787158799833ca7857b86693b5
Emmy <cyubahiroemmy12@gmail.com>
2026-02-14 14:12:55 +0200

    Updated

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
-const express = require ('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
-const dbUser = 'cyubahiroemmy12_db_user';
-const dbPass = 'A%40barca100%25';
-const cluster = 'BackendDB';
+const dbUser = "Emmy_user1";
+const dbPass = "Barca2026";
+const cluster = "backenddb";
 
-app.get('/', (req, res) => {
-    res.send("Hello from the Node API Server Updated");
+app.get("/", (req, res) => {
+  res.send("Hello from the Node API Server Updated");
 });
 
-app.post('/api/products', (req, res) => {
-    res.send("Data received");
+app.post("/api/products", (req, res) => {
+  res.send("Data received");
 });
 
-
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
-mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@${cluster}.mongodb.net/?appName=BackendDB`)
-    .then(() => console.log("Connected to database!"))
-    .catch((error) => console.error("Database connection failed:", error.message));
+mongoose
+  .connect(
+    `mongodb+srv://${dbUser}:${dbPass}@${cluster}.ugxeiln.mongodb.net/?appName=BackendDB`,
+  )
+  .then(() => {
+    console.log("Connected to database!");
+    const PORT = 3000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) =>{
+    console.error("Database connection failed:");
+    console.error("Error Code:", error.code);
+    console.error("Full Message:", error.message);
+  }
+  );
